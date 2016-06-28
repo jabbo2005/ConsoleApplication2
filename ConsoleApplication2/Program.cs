06763aca55bfdc93d8e19426e4d9db902b802ab4
jabbo2005 <jabbo2005@yahoo.com>
2016-06-28 09:07:37 -0400

    Add more output to Application 2.

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello from Console Application 2");
+            Console.WriteLine("More hello from Console Application 2");
             Console.Read();
         }
     }
